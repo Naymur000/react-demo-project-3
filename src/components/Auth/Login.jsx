f7275a30f1f458e2d1b4ec1e5932f5895753daba
Naymur000 <nayeemur000@gmail.com>
2026-03-08 01:39:47 +0600

    r

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -23,20 +23,20 @@ const Login = () => {
         }
         }
         className="flex flex-col items-center justify-center">
-          {/* <input
+          <input
           value={email}
           onChange={(e)=>{
             setEmail(e.target.value)
             console.log(e.target.value)
           }}
-          required className="border-2 border-emerald-500 rounded-2xl p-2 m-3 text-2xl outline-none " type="email" placeholder="Enter your email" /> */}
-          {/* <input
+          required className="border-2 border-emerald-500 rounded-2xl p-2 m-3 text-2xl outline-none " type="email" placeholder="Enter your email" />
+          <input
           value={password}
           onChange={(e)=>{
             setPassword(e.target.value)
           }}
-          required className="border-2 border-emerald-500 rounded-2xl p-2 m-1 text-2xl outline-none " type="password" placeholder="Enter your password" /> */}
-          {/* <button className="border-2 border-emerald-600 rounded-2xl py-2 px-6 m-3 text-xl outline-none bg-emerald-600" >Log in</button> */}
+          required className="border-2 border-emerald-500 rounded-2xl p-2 m-1 text-2xl outline-none " type="password" placeholder="Enter your password" />
+          <button className="border-2 border-emerald-600 rounded-2xl py-2 px-6 m-3 text-xl outline-none bg-emerald-600" >Log in</button>
         </form>
       </div>
     </div>
