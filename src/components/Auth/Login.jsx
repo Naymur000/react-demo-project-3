dc8e7eb2f767e1ee05b82434aeda2c3f897050df
Naymur000 <nayeemur000@gmail.com>
2026-03-17 08:31:53 +0600

    input

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -30,12 +30,12 @@ const Login = () => {
             console.log(e.target.value)
           }}
           required className="border-2 border-emerald-500 rounded-2xl p-2 m-3 text-2xl outline-none " type="email" placeholder="Enter your email" /> */}
-          <input
+          {/* <input
           value={password}
           onChange={(e)=>{
             setPassword(e.target.value)
           }}
-          required className="border-2 border-emerald-500 rounded-2xl p-2 m-1 text-2xl outline-none " type="password" placeholder="Enter your password" />
+          required className="border-2 border-emerald-500 rounded-2xl p-2 m-1 text-2xl outline-none " type="password" placeholder="Enter your password" /> */}
           <button className="border-2 border-emerald-600 rounded-2xl py-2 px-6 m-3 text-xl outline-none bg-emerald-600" >Log in</button>
         </form>
       </div>
