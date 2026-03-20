635611649c40ae9272df0ced01a3d6da6655d266
Naymur000 <nayeemur000@gmail.com>
2026-03-20 12:37:18 +0600

    i

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -39,7 +39,6 @@ const Login = () => {
           <button className="border-2 border-emerald-600 rounded-2xl py-2 px-6 m-3 text-xl outline-none bg-emerald-600" >Log in</button>
         </form>
       </div>
-      <h1>Hello</h1>
     </div>
   );
 };
